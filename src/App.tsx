84fbe662486ce4ddf68e409c4e39530aea0b14a7
Sjobergan <Oskar.filip.sjoberg@gmail.com>
2025-03-26 10:13:24 +0100

    Update deployment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import Hero from './components/Hero'
 import HomeCases from './components/HomeCases'
